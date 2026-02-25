365f76a34bff8a27e2824046480a69a6029863f1
Ayush Gupta <119103996+AYUSH0613@users.noreply.github.com>
2026-02-25 15:35:53 +0530

    Add initial App component with header and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
 
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
