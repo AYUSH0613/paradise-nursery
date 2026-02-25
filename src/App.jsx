f329413dd759e288cf5cfcbb187342e33d9ce387
Ayush Gupta <119103996+AYUSH0613@users.noreply.github.com>
2026-02-25 15:42:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
-import React from "react";
+import { useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
+  const [showProducts, setShowProducts] = useState(false);
+
   return (
     <div>
-      <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+      <h1>Welcome to Paradise Nursery</h1>
+
+      {!showProducts ? (
+        <button onClick={() => setShowProducts(true)}>
+          Get Started
+        </button>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 }
